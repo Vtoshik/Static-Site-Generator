2ebc26f08cd0b9b2a1072bf9e80282a0dae4b6d6
Vtoshik <duds3655@gmail.com>
2025-07-22 17:31:04 +0200

    add functions to split links and images from text and main function that uses all split function to split one big block of text into nodes with separate text type

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from htmlnode import LeafNode
+import re
 
 class TextType(Enum):
     TEXT = "text"
@@ -57,3 +58,83 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(part, text_type))
 
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        images = extract_markdown_images(node.text)
+        
+        if not images:
+            new_nodes.append(node)
+            continue
+
+        current_text = node.text
+
+        for alt_text, url in images:
+            markdown_image = f"![{alt_text}]({url})"
+            sections = current_text.split(markdown_image, 1)
+
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url=url))
+            current_text = sections[1]
+
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        links = extract_markdown_links(node.text)
+        
+        if not links:
+            new_nodes.append(node)
+            continue
+
+        current_text = node.text
+
+        for link_text, url in links:
+            markdown_link = f"[{link_text}]({url})"
+            sections = current_text.split(markdown_link, 1)
+
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(link_text, TextType.LINK, url=url))
+            current_text = sections[1]
+
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+    return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    
+    return nodes
